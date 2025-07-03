9d640dcea7ccca5be567b888893c1a6a3607d0e4
LENOVO <LENOVO@LAPTOP-ALTVFKV9>
2025-07-03 11:54:21 +0530

    Has A relation

---
+++ Day 5/src/HasARelation/Executor.java
@@ -0,0 +1,15 @@
+package HasARelation;
+
+public class Executor {
+	
+    
+	public static void main(String[] args) {
+		address obj1=new address("nearcollege","pune",410507);
+		
+		person obj2=new person("Kundan",8010);
+		
+		  obj2.display();
+		  
+	}
+	
+}
